56852dfeb5be3e34610ed4f91a99bb5545239ad7
Yusuke Kishishita <theshoreunder@gmail.com>
2019-07-30 12:45:39 +0900

    progress

---
+++ Weight_controll_suit_client/src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+#include <WiFi.h>
+const char *ssid = "ESP32-WiFi3"; //SSID of server
+const char *pass = "esp32wifi3";  //PW of server
+static WiFiClient client;        //Declear client
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.disconnect(true);
+    WiFi.begin(ssid, pass); //Connect to server
+    Serial.printf("\n");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(1000);
+    }
+    Serial.printf("\n");
+    Serial.println("WiFi Connected");
+    IPAddress ip(192, 168, 0, 10); //IP address of server
+    client.connect(ip, 80);       //Connect to IP address and port number
+}
+
+void loop()
+{
+  if (Serial.available() > 0) {
+    char serialSignal = Serial.read();
+    if (serialSignal == '0') {
+      client.println("a");
+    }
+    if (serialSignal == '1') {
+      client.println("b");
+    }
+    if (serialSignal == '2') {
+      client.println("c");
+    }
+    if (serialSignal == '3') {
+      client.println("d");
+    }
+    if (serialSignal == '4') {
+      client.println("e");
+    }
+    if (serialSignal == '5') {
+      client.println("f");
+    }
+    if (serialSignal == '6') {
+      client.println("g");
+    }
+    if (serialSignal == '7') {
+      client.println("h");
+    }
+    if (serialSignal == '8') {
+      client.println("i");
+    }
+    if (serialSignal == '9') {
+      client.println("j");
+    }
+  }
+}
---
+++ Weight_controll_suit_server/src/main.cpp
@@ -0,0 +1,85 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+int LED_pin = 4;
+
+// PGM pin
+//Extension
+int PGM_12 = 12, PGM_13 = 13, PGM_14 = 14, PGM_15 = 15, PGM_21 = 21;
+//Flexion
+int PGM_25 = 25, PGM_26 = 26, PGM_27 = 27, PGM_32 = 32, PGM_33 = 33;
+
+char value;
+
+const char *ssid = "ESP32-WiFi3";          //SSID
+const char *pass = "esp32wifi3";           //PW
+const IPAddress ip(192, 168, 0, 10);       //IP address of server
+const IPAddress subnet(255, 255, 255, 0); //Subnetmask
+WiFiServer server(80);
+
+void setup()
+{
+    Serial.begin(115200);
+    //pin setup
+    pinMode(LED_pin, OUTPUT);
+    pinMode(PGM_12, OUTPUT);
+    pinMode(PGM_13, OUTPUT);
+    pinMode(PGM_14, OUTPUT);
+    pinMode(PGM_15, OUTPUT);
+    pinMode(PGM_21, OUTPUT);
+    pinMode(PGM_25, OUTPUT);
+    pinMode(PGM_26, OUTPUT);
+    pinMode(PGM_27, OUTPUT);
+    pinMode(PGM_32, OUTPUT);
+    pinMode(PGM_33, OUTPUT);
+    WiFi.softAP(ssid, pass);           //Set the SSID and PW
+    delay(100);                        //Delay to avoid the deconnection
+    WiFi.softAPConfig(ip, ip, subnet); //Set IP address, Gateway, and Subnetmask
+    IPAddress myIP = WiFi.softAPIP();  //WiFi start
+    server.begin();                    //Server start
+    Serial.println();
+    /*各種情報を表示*/
+    Serial.println("WiFi connected");
+    Serial.print("SSID：");
+    Serial.println(ssid);
+    Serial.print("AP IP address：");
+    Serial.println(myIP);
+    Serial.println("Server start!");
+    digitalWrite(LED_pin, LOW);
+}
+
+void loop()
+{
+    WiFiClient client = server.available();
+    if (client)
+    {
+        Serial.println("New Client.");
+        while (client.connected())
+        {
+          //When WiFi connnection success, this pin is HIGH.
+          digitalWrite(LED_pin, HIGH);
+          value = client.read();
+          if(value == 'a'){
+            digitalWrite(PGM_12, HIGH);
+          }else if(value == 'b'){
+            digitalWrite(PGM_13, HIGH);
+          }else if(value == 'c'){
+            digitalWrite(PGM_14, HIGH);
+          }else if(value == 'd'){
+            digitalWrite(PGM_15, HIGH);
+          }else if(value == 'e'){
+            digitalWrite(PGM_21, HIGH);
+          }else if(value == 'f'){
+            digitalWrite(PGM_25, HIGH);
+          }else if(value == 'g'){
+            digitalWrite(PGM_26, HIGH);
+          }else if(value == 'h'){
+            digitalWrite(PGM_27, HIGH);
+          }else if(value == 'i'){
+            digitalWrite(PGM_32, HIGH);
+          }else if(value == 'j'){
+            digitalWrite(PGM_33, HIGH);
+          }
+        }
+    }
+}
